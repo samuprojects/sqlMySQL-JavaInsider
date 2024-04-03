21e7325d7695bda624606ee4f17371dcc66e55a4
Samuel <samugit.projects@gmail.com>
2024-04-03 10:43:02 -0300

    Aula10 O comando INSERT (Parte 1/2)

---
+++ P3-Inserindo_Excluindo_Atualizando_registros/ComandosAula10.sql
@@ -0,0 +1,15 @@
+#insert into airport values ('GRU', 'Aeroporto de Guarulhos', 'Brasil');
+
+SELECT * FROM traveldb.airport;
+
+insert into airport (iata_code, name, country) values ('LAX', 'Aeroporto de Los Angeles', 'EUA');
+
+SELECT * FROM traveldb.airport;
+
+insert into customer (name, email) values ('Carlos', 'a@a.com');
+
+SELECT * FROM traveldb.customer;
+
+insert into customer (name, email) values ('Pedro', 'b@b.com');
+
+SELECT * FROM traveldb.customer;
