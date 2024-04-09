7a44f9479f60602bf0b6aebdedca7773ef3d2dcb
Samuel <samugit.projects@gmail.com>
2024-04-08 23:53:48 -0300

    Comandos sql aula 13

---
+++ P3-Inserindo_Excluindo_Atualizando_registros/ComandosAula13.sql
@@ -0,0 +1,11 @@
+SELECT * FROM traveldb.customer;
+
+update customer set email = 'sam@a.com', name = 'Samu' where id = 1;
+
+SELECT * FROM traveldb.customer;
+
+SELECT * FROM traveldb.flight;
+
+update flight set price  = 410.0 where number = 1234;
+
+SELECT * FROM traveldb.flight;
